6a17df27adcb8cfe2bd5f3f862206076f4b74735
gouez <lyricsong47@gmail.com>
2023-06-14 00:03:25 +0800

    format code

--- src/Core/Framework/Api/Controller/CacheController.php
+++ src/Core/Framework/Api/Controller/CacheController.php
@@ -7,7 +7,6 @@ use Shuwei\Core\Framework\DataAbstractionLayer\Indexing\EntityIndexerRegistry;
 use Shuwei\Core\Framework\Log\Package;
 use Shuwei\Core\Framework\Util\Random;
 use Shuwei\Core\Framework\Validation\DataBag\RequestDataBag;
-use Shuwei\Storefront\Framework\Cache\CacheWarmer\CacheWarmer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\Cache\Adapter\TagAwareAdapter;
@@ -26,12 +25,11 @@ class CacheController extends AbstractController
     public function __construct(
         private readonly CacheClearer $cacheClearer,
         private readonly AdapterInterface $adapter,
-        private readonly ?CacheWarmer $cacheWarmer,
         private readonly EntityIndexerRegistry $indexerRegistry
     ) {
     }
 
-    #[Route(path: '/api/_action/cache_info', name: 'api.action.cache.info', methods: ['GET'], defaults: ['_acl' => ['system:cache:info']])]
+    #[Route(path: '/api/_action/cache_info', name: 'api.action.cache.info', defaults: ['_acl' => ['system:cache:info']], methods: ['GET'])]
     public function info(): JsonResponse
     {
         return new JsonResponse([
@@ -41,7 +39,7 @@ class CacheController extends AbstractController
         ]);
     }
 
-    #[Route(path: '/api/_action/index', name: 'api.action.cache.index', methods: ['POST'], defaults: ['_acl' => ['api_action_cache_index']])]
+    #[Route(path: '/api/_action/index', name: 'api.action.cache.index', defaults: ['_acl' => ['api_action_cache_index']], methods: ['POST'])]
     public function index(RequestDataBag $dataBag): Response
     {
         $data = $dataBag->all();
@@ -52,19 +50,7 @@ class CacheController extends AbstractController
         return new Response('', Response::HTTP_NO_CONTENT);
     }
 
-    #[Route(path: '/api/_action/cache_warmup', name: 'api.action.cache.delete_and_warmup', methods: ['DELETE'], defaults: ['_acl' => ['system:clear:cache']])]
-    public function clearCacheAndScheduleWarmUp(): Response
-    {
-        if ($this->cacheWarmer === null) {
-            throw new \RuntimeException('Storefront is not installed');
-        }
-
-        $this->cacheWarmer->warmUp(Random::getAlphanumericString(32));
-
-        return new Response('', Response::HTTP_NO_CONTENT);
-    }
-
-    #[Route(path: '/api/_action/cache', name: 'api.action.cache.delete', methods: ['DELETE'], defaults: ['_acl' => ['system:clear:cache']])]
+    #[Route(path: '/api/_action/cache', name: 'api.action.cache.delete', defaults: ['_acl' => ['system:clear:cache']], methods: ['DELETE'])]
     public function clearCache(): Response
     {
         $this->cacheClearer->clear();
@@ -72,7 +58,7 @@ class CacheController extends AbstractController
         return new Response('', Response::HTTP_NO_CONTENT);
     }
 
-    #[Route(path: '/api/_action/cleanup', name: 'api.action.cache.cleanup', methods: ['DELETE'], defaults: ['_acl' => ['system:clear:cache']])]
+    #[Route(path: '/api/_action/cleanup', name: 'api.action.cache.cleanup', defaults: ['_acl' => ['system:clear:cache']], methods: ['DELETE'])]
     public function clearOldCacheFolders(): Response
     {
         $this->cacheClearer->scheduleCacheFolderCleanup();
@@ -80,7 +66,7 @@ class CacheController extends AbstractController
         return new Response('', Response::HTTP_NO_CONTENT);
     }
 
-    #[Route(path: '/api/_action/container_cache', name: 'api.action.container-cache.delete', methods: ['DELETE'], defaults: ['_acl' => ['system:clear:cache']])]
+    #[Route(path: '/api/_action/container_cache', name: 'api.action.container-cache.delete', defaults: ['_acl' => ['system:clear:cache']], methods: ['DELETE'])]
     public function clearContainerCache(): Response
     {
         $this->cacheClearer->clearContainerCache();
@@ -92,7 +78,7 @@ class CacheController extends AbstractController
     {
         if ($adapter instanceof TagAwareAdapter || $adapter instanceof TraceableAdapter) {
             // Do not declare function as static
-            $func = \Closure::bind(fn () => $adapter->pool, $adapter, $adapter::class);
+            $func = \Closure::bind(fn () => $adapter->getPool(), $adapter, $adapter::class);
 
             $adapter = $func();
         }
