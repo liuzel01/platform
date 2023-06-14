f385d1862a83478aca7ce02dbe0bf7ec779bd1c0
gouez <lyricsong47@gmail.com>
2023-06-14 21:05:53 +0800

    git workflows

--- src/Administration/Resources/app/administration/jest.config.js
+++ src/Administration/Resources/app/administration/jest.config.js
@@ -69,7 +69,7 @@ module.exports = {
         // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports.
         // See https://github.com/uuidjs/uuid/issues/451
         '^uuid$': require.resolve('uuid'),
-        '^\@shuwei-ag\/admin-extension-sdk\/es\/(.*)': '<rootDir>/node_modules/@58shuwei-ag/admin-extension-sdk/umd/$1',
+        '^\@58shuwei-ag\/admin-extension-sdk\/es\/(.*)': '<rootDir>/node_modules/@58shuwei-ag/admin-extension-sdk/umd/$1',
         '^lodash-es$': 'lodash',
     },
 
